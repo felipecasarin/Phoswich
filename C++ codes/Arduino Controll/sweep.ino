c8cc52f4b393147a0ab39d4093b8f1b13d7c7b9d
Felipe Casarin <casarin@df.ufscar.br>
2022-08-01 15:50:04 -0300

    sweep.ino codes from August 1, 2022

--- C++ codes/Arduino Controll/sweep.ino
+++ C++ codes/Arduino Controll/sweep.ino
@@ -1,3 +1,6 @@
+int ENA1=4; //define Enable pin
+int ENA2=5; //define Enable pin
+
 int PUL1=7; //define Pulse pin
 int DIR1=6; //define Direction pin
 
@@ -25,44 +28,52 @@ void ttl_long_off(){
 
 void for_x(){          //Moves 1mm in the positive x direction
   for (int k=0; k<252; k++){
+    digitalWrite(ENA1,HIGH);
     digitalWrite(DIR1,LOW);
     digitalWrite(PUL1,HIGH);
     delayMicroseconds(500);
     digitalWrite(PUL1,LOW);
     delayMicroseconds(500);
+    digitalWrite(ENA1,LOW);
   }
   
 }
 
 void back_x(){          //Moves 1mm in the negative x direction
   for (int k=0; k<252; k++){
+    digitalWrite(ENA1,HIGH);
     digitalWrite(DIR1,HIGH);
     digitalWrite(PUL1,HIGH);
     delayMicroseconds(500);
     digitalWrite(PUL1,LOW);
     delayMicroseconds(500);
+    digitalWrite(ENA1,LOW);
   }
   
 }
 
 void for_y(){         //Moves 1mm in the positive y direction
   for (int k=0; k<252; k++){
+    digitalWrite(ENA2,HIGH);
     digitalWrite(DIR2,LOW);
     digitalWrite(PUL2,HIGH);
     delayMicroseconds(500);
     digitalWrite(PUL2,LOW);
     delayMicroseconds(500);
+    digitalWrite(ENA2,LOW);
   }
   
 }
 
 void back_y(){          //Moves 1mm in the negative y direction
   for (int k=0; k<252; k++){
+    digitalWrite(ENA2,HIGH);
     digitalWrite(DIR2,HIGH);
     digitalWrite(PUL2,HIGH);
     delayMicroseconds(500);
     digitalWrite(PUL2,LOW);
     delayMicroseconds(500);
+    digitalWrite(ENA2,LOW);
   }
   
 }
@@ -81,13 +92,13 @@ void setup() {
       
     {
       for_x();
-      ttl_long_on();		//Start long TTL pulse (up)
-      ttl_short();		//One 1us signal before starting acquisition
+      ttl_long_on();    //Start long TTL pulse (up)
+      ttl_short();    //One 1us signal before starting acquisition
       
-      delay(1000);		//Data acquisition time
+      delay(1000);    //Data acquisition time
       
-      ttl_short();		//One 1us signal to finish acquisition
-      ttl_long_off();		//Finish long TTL pulse (down)
+      ttl_short();    //One 1us signal to finish acquisition
+      ttl_long_off();   //Finish long TTL pulse (down)
       
     }
     
@@ -98,14 +109,21 @@ void setup() {
     }
     
     for_y();
+    ttl_long_on();    //Start long TTL pulse (up)
+    ttl_short();    //One 1us signal before starting acquisition
+      
+    delay(1000);    //Data acquisition time
+      
+    ttl_short();    //One 1us signal to finish acquisition
+    ttl_long_off();   //Finish long TTL pulse (down)
     
     
   } 
 
-    for (int j=0; j<11; j++)
+    for (int j=0; j<10; j++)
       
     {
-      back_y();      
+      back_y();      //Go back to starting point
     }
     
 
